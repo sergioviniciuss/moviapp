3b09290a23508bb9682dc405ebe12857c2cd90cc
sergioviniciuss <sergioviniciuss@gmail.com>
2017-10-22 08:37:27 -0200

    setting up discover suggestions url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,4 @@
 export const API_KEY = "ffacbe5e551a0ea175ff36610debfde9";
 export const API_SEARCH_URL = `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=`;
 export const API_IMG_DOMAIN = "https://image.tmdb.org/t/p/w370_and_h556_bestv2/"
-// export const API_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZmFjYmU1ZTU1MWEwZWExNzVmZjM2NjEwZGViZmRlOSIsInN1YiI6IjU5ZWJlYjJlOTI1MTQxMTkxYTAwMGYwMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qtcYSQeO3g7QC2bAyR8ATv3jHAyDblOcXl5mPVgUgOY";
-// export const API_URL = `https://api.themoviedb.org/3/movie/76341?api_key=${API_KEY}`;
-// export const API_URL_DISCOVER_SUGGESTIONS = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=release_date.asc&include_adult=false&include_video=false&page=1&year=2017`;
-
+export const API_URL_DISCOVER_MOST_RANKED = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&year=2017`
