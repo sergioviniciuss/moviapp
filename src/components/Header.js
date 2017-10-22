6b54ffedb5b550299cdda2606ef1aaa6c6f91024
sergioviniciuss <sergioviniciuss@gmail.com>
2017-10-22 14:26:05 -0200

    Fixing horizontal scroll

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Grid } from 'react-bootstrap';
 import logo from '../images/logo-green.png';
 import './Header.css';
 
 class Header extends Component {
     render () {
         return (
-            <Row className="show-grid header">
-				<Col md={12}>
-					<img src={logo} className="app-logo" alt="logo" />
-				</Col>
-				<Col md={12}>
-					<h1 className="main-title">The best place to find information about your favorite movies</h1>
-				</Col>
-            </Row>
+            <Grid className="full-width header">
+                <Row className="show-grid">
+                    <Col md={12}>
+                        <img src={logo} className="app-logo" alt="logo" />
+                    </Col>
+                    <Col md={12}>
+                        <h1 className="main-title">The best place to find information about your favorite movies</h1>
+                    </Col>
+                </Row>
+            </Grid>
         )
     }
 }
