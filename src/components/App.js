ccd4d1502921074fe5f237e1076fcf8136c6f894
sergioviniciuss <sergioviniciuss@gmail.com>
2017-10-22 14:24:41 -0200

    cleaning code

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
 import Header from './Header';
 import Body from './Body';
 
 class App extends Component {
     render () {
         return (
-            <div className="">
+            <div>
                 <Header />
                 <Body />
             </div>
