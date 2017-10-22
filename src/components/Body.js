d18cb1db133638017cc199647a9cb5c01a8a4deb
sergioviniciuss <sergioviniciuss@gmail.com>
2017-10-22 02:48:31 -0200

    Using axios to fech API and get results for the searched word

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col, Panel, Form, ControlLabel, FormGroup, FormControl, Button } from 'react-bootstrap';
+import axios from 'axios';
+import {API_KEY, API_SEARCH_URL} from '../config'
 import './Body.css';
-import SearchForm from './SearchForm';
 
 
 class Body extends Component {
@@ -16,9 +17,35 @@ class Body extends Component {
 		event.preventDefault();
 		console.log("clicked");
 		console.log("searchedWord ", this.state.searchedWord);
+		this.getSearchResults();
 	}
 	getSearchResults() {
-
+		axios.get(`${API_SEARCH_URL}${this.state.searchedWord}`)
+        .then(res => {
+            if (!!res && res.hasOwnProperty('data')) {
+				const data = res.data;
+				console.log(data);
+                this.setState(data);
+            }
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+            }
+			console.log(error.config);
+        });
 	}
     render () {
         return (
