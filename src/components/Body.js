5abf93ab831914abe285b3243ee519c089d68fa7
sergioviniciuss <sergioviniciuss@gmail.com>
2017-10-22 02:17:50 -0200

    Adding search form to Body component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Panel, Form, ControlLabel, FormGroup, FormControl, Button } from 'react-bootstrap';
+import './Body.css';
+
 
 class Body extends Component {
     render () {
         return (
             <Grid>
                 <Row className="show-grid">
-					<Col md={12}>
-						search
+					<Col md={12} className="center-align">
+						<Panel className="main-bg-color custom-panel" header="MOVIE DATA SEARCH">
+						<Form inline>
+							<FormGroup controlId="formInlineName">
+								<ControlLabel>Search for:</ControlLabel>
+								{' '}
+								<FormControl className="custom-input" type="search" placeholder="movie keywords" />
+							</FormGroup>
+							{' '}
+							<Button bsStyle="success" type="submit">
+								Search
+							</Button>
+						</Form>
+						</Panel>
 					</Col>
 					<Col md={12}>
 						search result
